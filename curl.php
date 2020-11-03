1b65b152a2137f0d0aaccdecacb49c3456908b6f
Muhammad Zen <54342923+ZenBahsin@users.noreply.github.com>
2020-11-03 16:21:08 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -7,10 +7,6 @@ function http_request($url){
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
     $output = curl_exec($ch);
 
     curl_close($ch);
